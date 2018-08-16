4f392667d9dc25bfd741705c78efc7e1ce3fedf4
yuxu <yzx105psu@gmail.com>
2018-08-16 09:16:29 -0700

    Initial commit

---
+++ src/main/java/edu/sjsu/xuy87/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package edu.sjsu.xuy87.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/edu/sjsu/xuy87/springpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package edu.sjsu.xuy87.springpetclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public PetType() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
