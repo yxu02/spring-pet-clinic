db79e3291161e68ffdf2515d5e2d35ed78a1297e
yuxu <yzx105psu@gmail.com>
2018-08-17 20:22:27 -0700

    Add static resources

--- pet-clinic-web/src/main/java/edu/sjsu/xuy87/springpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/edu/sjsu/xuy87/springpetclinic/controller/VetController.java
@@ -1,14 +1,24 @@
 package edu.sjsu.xuy87.springpetclinic.controller;
 
+import edu.sjsu.xuy87.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vet")
 @Controller
 public class VetController {
 
+    VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "index", "index.html"})
-    public String vetList(){
+    public String vetList(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
         return "vet/index";
     }
 }
