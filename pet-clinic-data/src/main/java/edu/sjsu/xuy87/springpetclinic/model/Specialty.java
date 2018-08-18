c80268f161648a3ddd74500cc787a9b5c0f9f010
yuxu <yzx105psu@gmail.com>
2018-08-17 21:11:08 -0700

    Add additional entities

---
+++ pet-clinic-data/src/main/java/edu/sjsu/xuy87/springpetclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package edu.sjsu.xuy87.springpetclinic.model;
+
+public class Specialty extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
